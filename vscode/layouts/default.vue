bdbe61eab7272daa364bbc6d7190cc46681cbabb
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-03-13 23:02:36 +0000

    Factor components into layout

--- vscode/layouts/default.vue
+++ vscode/layouts/default.vue
@@ -1,55 +1,23 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="min-w-full bg-gray-500 mx-auto">
+    <div class="w-full bg-gray-600">
+      <TitleBar />
+      <div class="flex flex-1 w-full bg-gray-700">
+        <Sidebar />
+        <Explorer />
+        <div id="main" class="flex-col w-full bg-gray-900">
+          <Tabs />
+          <Nuxt />
+          <Terminal />
+        </div>
+      </div>
+      <FooterBar />
+    </div>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+export default {}
+</script>
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+<style></style>
