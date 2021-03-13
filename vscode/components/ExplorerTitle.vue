531b1b7d93d2ba8dfb86b9813cb1a1ec6f481455
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-03-13 23:01:27 +0000

    Create explorer components

---
+++ vscode/components/ExplorerTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="flex justify-between items-center text-sm text-gray-100 px-3 py-2"
+  >
+    <div class="font-light">Explorer</div>
+    <div class="w-5 text-gray-200">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-more-horizontal"
+      >
+        <circle cx="12" cy="12" r="1"></circle>
+        <circle cx="19" cy="12" r="1"></circle>
+        <circle cx="5" cy="12" r="1"></circle>
+      </svg>
+    </div>
+  </div>
+</template>
