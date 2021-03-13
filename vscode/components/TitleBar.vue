d0dd0380d9c4e3f0260a956fec15956408ae7b4f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-03-13 23:01:51 +0000

    Create title bar component

---
+++ vscode/components/TitleBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav class="flex ustify-between items-center px-4 py-1">
+    <ul class="flex flex-1 items-center space-x-4 text-gray-200 text-sm">
+      <li>File</li>
+      <li>Edit</li>
+      <li>Selection</li>
+      <li>View</li>
+      <li>Go</li>
+      <li>Run</li>
+      <li>Terminal</li>
+      <li>Help</li>
+    </ul>
+    <ul class="flex flex-1 text-gray-100 text-sm">
+      <li class="flex flex-none text-xs">
+        index.html - vscode.lloyd.cx - Visual Studio Code
+      </li>
+    </ul>
+    <ul class="flex justify-end space-x-5 text-gray-100 text-sm">
+      <li class="h-4 w-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-minus"
+        >
+          <line x1="5" y1="12" x2="19" y2="12"></line>
+        </svg>
+      </li>
+      <li class="h-4 w-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-square"
+        >
+          <rect x="3" y="7" width="18" height="12" rx="1" ry="1"></rect>
+        </svg>
+      </li>
+      <li class="h-4 w-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-x"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
